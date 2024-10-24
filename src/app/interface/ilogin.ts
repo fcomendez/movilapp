a7adf13c380ab66622afba7487b343f627a1ff80
CETECOM <cetecom_conce@duoc.cl>
2024-10-24 10:30:49 -0300

    vamosaver

---
+++ src/app/interface/ilogin.ts
@@ -0,0 +1,11 @@
+export interface Login{
+    Login: Login[];
+  }
+  export interface Login {
+    idCategory: number;
+    nombre: string;
+    apellido: string;
+    numeroTelefono: string;
+    correo: string;
+    contrasena: string;
+  }
